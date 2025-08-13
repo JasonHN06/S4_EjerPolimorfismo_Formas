9ad4425db3809fd8881a0ee69a4bc503f2100525
Jason Madrid <jasonhnn6@gmail.com>
2025-08-13 16:42:43 -0600

    adding data

--- EjerPolimorfismo_Formas/Program.cs
+++ EjerPolimorfismo_Formas/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola!, Jason");
+﻿using EjerPolimorfismo_Formas;
+
+Circulo circulo = new Circulo(5.0);
+Rectangulo rectangulo = new Rectangulo(4.0, 6.0);
+
+List<Forma> formas = new List<Forma>();
+formas.Add(circulo);
+formas.Add(rectangulo);
+
+foreach (Forma forma in formas)
+{
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine("***** Resultados *****");
+    Console.WriteLine($"Forma: {forma.Nombre} - Área: {forma.CalcularArea():F2}");
+}
+Console.WriteLine();
